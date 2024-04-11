ad0844fc6d0ea038f7f5a02c0a0e2887263c17f3
LouisMichelin <louis.michelin097@gmail.com>
2024-04-11 16:26:45 +0200

    Sauvegarde

--- src/components/Daily/Daily.jsx
+++ src/components/Daily/Daily.jsx
@@ -21,37 +21,37 @@ function Daily() {
    let defaultValues = [
       {
          day: "2024-01-01",
-         kilogram: 50,
+         kilogram: 90,
          calories: 100,
       },
       {
          day: "2024-01-02",
-         kilogram: 50,
+         kilogram: 80,
          calories: 200,
       },
       {
          day: "2024-01-03",
-         kilogram: 45,
+         kilogram: 70,
          calories: 300,
       },
       {
          day: "2024-01-04",
-         kilogram: 45,
+         kilogram: 60,
          calories: 400,
       },
       {
          day: "2024-01-05",
-         kilogram: 40,
+         kilogram: 55,
          calories: 500,
       },
       {
          day: "2024-01-06",
-         kilogram: 45,
+         kilogram: 50,
          calories: 600,
       },
       {
          day: "2024-01-07",
-         kilogram: 40,
+         kilogram: 45,
          calories: 700,
       },
    ];
@@ -77,7 +77,7 @@ function Daily() {
       });
    }
 
-   // CUSTOMIZED TOOLTIP
+   // // CUSTOMIZED TOOLTIP
    const CustomTooltip = ({ active, payload }) => {
       if (active && payload && payload.length) {
          return (
@@ -105,73 +105,113 @@ function Daily() {
       }
       return null;
    };
-   // CUSTOMIZED LEGEND
-   const RenderCustomizedLegend = () => {
-      return (
-         <div className="LegendWrapper">
-            <div className="LegendPoids">
-               <div
-                  className="LegendCircle"
-                  style={{ backgroundColor: "black" }}
-               ></div>
-               <div>Poids (kg)</div>
-            </div>
-            <div className="LegendCalories">
-               <div
-                  className="LegendCircle"
-                  style={{ backgroundColor: "red" }}
-               ></div>
-               <div>Calories brûlées (kCal)</div>
-            </div>
-         </div>
-      );
-   };
-   // CUSTOMIZED AXE X
-   const CustomizedAxisTickX = ({ x, y, payload }) => {
-      return (
-         <g transform={`translate(${x},${y})`}>
-            <text
-               x={0}
-               y={0}
-               dy={25}
-               fill="#9B9EAC"
-               style={{ fontWeight: "600", fontSize: "14px" }}
-            >
-               {payload.value}
-            </text>
-         </g>
-      );
-   };
-   // CUSTOMIZED AXE Y
-   const CustomizedAxisTickY = ({ x, y, payload }) => {
-      return (
-         <g transform={`translate(${x},${y})`}>
-            <text
-               x={0}
-               y={0}
-               dy={5}
-               dx={30}
-               fill="#9B9EAC"
-               style={{ fontWeight: "600", fontSize: "14px" }}
-            >
-               {payload.value}
-            </text>
-         </g>
-      );
-   };
+   // // CUSTOMIZED LEGEND
+   // const RenderCustomizedLegend = () => {
+   //    return (
+   //       <div className="LegendWrapper">
+   //          <div className="LegendPoids">
+   //             <div
+   //                className="LegendCircle"
+   //                style={{ backgroundColor: "black" }}
+   //             ></div>
+   //             <div>Poids (kg)</div>
+   //          </div>
+   //          <div className="LegendCalories">
+   //             <div
+   //                className="LegendCircle"
+   //                style={{ backgroundColor: "red" }}
+   //             ></div>
+   //             <div>Calories brûlées (kCal)</div>
+   //          </div>
+   //       </div>
+   //    );
+   // };
+   // // CUSTOMIZED AXE X
+   // const CustomizedAxisTickX = ({ x, y, payload }) => {
+   //    return (
+   //       <g transform={`translate(${x},${y})`}>
+   //          <text
+   //             x={0}
+   //             y={0}
+   //             dy={25}
+   //             fill="#9B9EAC"
+   //             style={{ fontWeight: "600", fontSize: "14px" }}
+   //          >
+   //             {payload.value}
+   //          </text>
+   //       </g>
+   //    );
+   // };
+   // // CUSTOMIZED AXE Y
+   // const CustomizedAxisTickY = ({ x, y, payload }) => {
+   //    return (
+   //       <g transform={`translate(${x},${y})`}>
+   //          <text
+   //             x={0}
+   //             y={0}
+   //             dy={5}
+   //             dx={30}
+   //             fill="#9B9EAC"
+   //             style={{ fontWeight: "600", fontSize: "14px" }}
+   //          >
+   //             {payload.value}
+   //          </text>
+   //       </g>
+   //    );
+   // };
+
+   // TEEEEEEEEEEEEEEEEEEEEEEEEEEST
+   const data = [
+      {
+         name: "Page A",
+         uv: 4000,
+         pv: 2400,
+         amt: 2400,
+      },
+      {
+         name: "Page B",
+         uv: 3000,
+         pv: 1398,
+         amt: 2210,
+      },
+      {
+         name: "Page C",
+         uv: 2000,
+         pv: 9800,
+         amt: 2290,
+      },
+      {
+         name: "Page D",
+         uv: 2780,
+         pv: 3908,
+         amt: 2000,
+      },
+      {
+         name: "Page E",
+         uv: 1890,
+         pv: 4800,
+         amt: 2181,
+      },
+      {
+         name: "Page F",
+         uv: 2390,
+         pv: 3800,
+         amt: 2500,
+      },
+      {
+         name: "Page G",
+         uv: 3490,
+         pv: 4300,
+         amt: 2100,
+      },
+   ];
 
    return (
       <div className="DailyWrapper">
          <div className="DailyTitle">Activité quotidienne</div>
-         <ResponsiveContainer width="100%" height="100%">
+         {/* <ResponsiveContainer width="100%" height="100%">
             <BarChart
                data={graphData}
-               // margin={{
-               //    top: 0,
-               //    right: 0,
-               //    left: 0,
-               //    bottom: 0,
-               // }}
                margin={{
                   top: 30,
                   right: 10,
@@ -198,7 +238,9 @@ function Daily() {
                   tick={CustomizedAxisTickY}
                   type="number"
                   interval={1}
+                  // domain={["min", "auto"]}
                   hide
+                  
                />
                <YAxis
                   dataKey={"poids"}
@@ -218,7 +260,6 @@ function Daily() {
                   cursor={{ fill: "#C4C4C480" }}
                />
                <Bar
-                  // yAxisId="left"
                   color="#74798c"
                   name="Poids (kg)"
                   dataKey="poids"
@@ -226,13 +267,42 @@ function Daily() {
                   radius={[4.5, 4.5, 0, 0]}
                />
                <Bar
-                  // yAxisId="right"
                   color="#74798c"
                   name="Calories brûlées (kCal)"
                   dataKey="calories"
                   fill="red"
                   radius={[4.5, 4.5, 0, 0]}
                />
+            </BarChart>
+         </ResponsiveContainer> */}
+         <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+               data={graphData}
+               margin={{
+                  top: 30,
+                  right: 10,
+                  left: 50,
+                  bottom: 5,
+               }}
+               barSize={10}
+               barGap={8}
+            >
+               <CartesianGrid strokeDasharray="2" />
+               <XAxis dataKey="day" />
+               <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
+               <YAxis
+                  yAxisId="right"
+                  orientation="right"
+                  stroke="#82ca9d"
+                  domain={["min", "auto"]}
+               />
+               <Tooltip
+                  content={CustomTooltip}
+                  cursor={{ fill: "#C4C4C480" }}
+               />
+               <Legend />
+               <Bar yAxisId="left" dataKey="calories" fill="red" />
+               <Bar yAxisId="right" dataKey="poids" fill="black" />
             </BarChart>
          </ResponsiveContainer>
       </div>
